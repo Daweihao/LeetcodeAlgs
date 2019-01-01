aba1622d23fde82098f3226bdcd7ff60022adb08
Daweihao <421679009@qq.com>
2019-01-01 17:54:53 -0500

    771--improved version.

--- 771/src/Solution.java
+++ 771/src/Solution.java
@@ -2,10 +2,9 @@ public class Solution {
     public int numJewelsInStones(String J, String S) {
         if (J == null || J.length() == 0 || S == null || S.length() ==0)
             return 0;
-        String[] stones = S.split("");
         int counts = 0;
-        for (int i=0; i < S.length(); i++){
-            if (J.contains(stones[i])){
+        for (int i = 0; i < S.length(); i++){
+            if (J.indexOf(S.charAt(i)) > -1){
                 counts++;
             }
         }
